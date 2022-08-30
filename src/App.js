cc2039af077deafe34cacb499664809c7ee93f22
Antônio Santana <antonionetosg6@gmail.com>
2022-08-30 15:09:11 -0300

    Requisito 1 - Importando Switch e organizando

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -13,15 +13,15 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <p>TrybeTunes</p>
-        <switch>
+        <Switch>
           <Route exact path="/" component={ Login } />
           <Route path="/search" component={ Search } />
           <Route path="/album:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
           <Route path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
-          {/* <Route exact path="*" component={NotFound} /> */}
-        </switch>
+          <Route exact path="/*" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
