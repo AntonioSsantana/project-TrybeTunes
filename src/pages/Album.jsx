6b7e1e03160fcaf769138da7e3c7632bfe7643e4
Antônio Santana <antonionetosg6@gmail.com>
2022-08-30 15:07:08 -0300

    Requisito 1 - Criando componentes e as rotas

---
+++ src/pages/Album.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-album">
+        <h2>Albuns:</h2>
+      </div>
+    );
+  }
+}
+
+export default Album;
