45e880281d928af8458f759bac2e90889bf2c936
Antônio Santana <antonionetosg6@gmail.com>
2022-09-13 13:07:18 -0300

    Requisito 9 e 10

--- src/pages/components/MusicCard.jsx
+++ src/pages/components/MusicCard.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { addSong } from '../../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs } from '../../services/favoriteSongsAPI';
 import Loading from './Loading';
 
 class MusicCard extends React.Component {
   state = {
     CHECKED: false,
     IS_LOADING: false,
+  };
+
+  async componentDidMount() {
+    this.handleFavorites();
+  }
+
+  handleFavorites = async () => {
+    this.setState({
+      IS_LOADING: true,
+    });
+    const FAVORITES_LIST = await getFavoriteSongs();
+    this.setState({
+      IS_LOADING: false,
+    });
+    const { trackId } = this.props;
+    const LIST = FAVORITES_LIST.some((i) => i.trackId === trackId);
+    this.setState({
+      CHECKED: LIST,
+    });
   };
 
   handleCheckbox = async (song) => {
@@ -27,32 +46,23 @@ class MusicCard extends React.Component {
     const { IS_LOADING, CHECKED } = this.state;
 
     return (
-      /*  <div>
-         <p>{trackName}</p>
-         <audio data-testid="audio-component" src={previewUrl} controls>
-           <track kind="captions" />
-           O seu navegador não suporta o elemento
-           <code>audio</code>
-           .
-         </audio>
-       </div> */
       IS_LOADING ? <Loading /> : (
         <div>
           <span>{trackName}</span>
-          <audio data-testid="audio-component" src={ previewUrl } controls>
+          <audio data-testid="audio-component" src={previewUrl} controls>
             <track kind="captions" />
             O seu navegador não suporta o elemento
             <code>audio</code>
             .
           </audio>
-          <label htmlFor={ trackId }>
+          <label htmlFor={trackId}>
             Favoritar
             <input
               type="checkbox"
-              data-testid={ `checkbox-music-${trackId}` }
-              id={ trackId }
-              checked={ CHECKED }
-              onChange={ () => this.handleCheckbox(music) }
+              data-testid={`checkbox-music-${trackId}`}
+              id={trackId}
+              checked={CHECKED}
+              onChange={() => this.handleCheckbox(music)}
             />
           </label>
         </div>
