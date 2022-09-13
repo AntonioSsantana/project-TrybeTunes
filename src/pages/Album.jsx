18954f19ebdef052e0604d8ced71130639d27788
Antônio Santana <antonionetosg6@gmail.com>
2022-09-13 13:02:09 -0300

    Requisito 8

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import Header from './components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from './components/MusicCard';
+import Loading from './components/Loading';
 
 class Album extends React.Component {
   state = {
     ALBUM_INFORMATION: {},
     SONGS: [],
+    IS_LOADING: false,
   };
 
   componentDidMount() {
@@ -24,26 +26,34 @@ class Album extends React.Component {
   };
 
   render() {
-    const { ALBUM_INFORMATION, SONGS } = this.state;
+    const { ALBUM_INFORMATION, SONGS, IS_LOADING } = this.state;
 
     return (
       <div data-testid="page-album">
         <Header />
         <h4>Album</h4>
-        <main>
-          <img
-            src={ ALBUM_INFORMATION.artworkUrl100 }
-            alt={ ALBUM_INFORMATION.collectionName }
-          />
-          <h2 data-testid="album-name">{ALBUM_INFORMATION.collectionName}</h2>
-          <h4 data-testid="artist-name">{ALBUM_INFORMATION.artistName}</h4>
-          {SONGS.map((i) => (
-            <div key={ i.trackId }>
-              <MusicCard trackName={ i.trackName } previewUrl={ i.previewUrl } />
+        {IS_LOADING ? <Loading /> : (
+          <main>
+            <img
+              src={ ALBUM_INFORMATION.artworkUrl100 }
+              alt={ ALBUM_INFORMATION.collectionName }
+            />
+            <div>
+              <h2 data-testid="album-name">{ALBUM_INFORMATION.collectionName}</h2>
+              <h4 data-testid="artist-name">{ALBUM_INFORMATION.artistName}</h4>
             </div>
-          ))}
-
-        </main>
+            {SONGS.map((i) => (
+              <div key={ i.trackId }>
+                <MusicCard
+                  trackName={ i.trackName }
+                  previewUrl={ i.previewUrl }
+                  trackId={ i.trackId }
+                  music={ i }
+                />
+              </div>
+            ))}
+          </main>
+        )}
       </div>
     );
   }
--- src/pages/components/MusicCard.jsx
+++ src/pages/components/MusicCard.jsx
@@ -1,26 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends React.Component {
+  state = {
+    CHECKED: false,
+    IS_LOADING: false,
+  };
+
+  handleCheckbox = async (song) => {
+    this.setState({
+      IS_LOADING: true,
+    });
+    await addSong(song);
+    this.setState({
+      IS_LOADING: false,
+      CHECKED: true,
+    });
+  };
+
   render() {
-    const { trackName, previewUrl } = this.props;
+    const { trackName, previewUrl,
+      trackId, music } = this.props;
+
+    const { IS_LOADING, CHECKED } = this.state;
+
     return (
-      <div>
-        <p>{ trackName }</p>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          <code>audio</code>
-          .
-        </audio>
-      </div>
+      /*  <div>
+         <p>{trackName}</p>
+         <audio data-testid="audio-component" src={previewUrl} controls>
+           <track kind="captions" />
+           O seu navegador não suporta o elemento
+           <code>audio</code>
+           .
+         </audio>
+       </div> */
+      IS_LOADING ? <Loading /> : (
+        <div>
+          <span>{trackName}</span>
+          <audio data-testid="audio-component" src={ previewUrl } controls>
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            <code>audio</code>
+            .
+          </audio>
+          <label htmlFor={ trackId }>
+            Favoritar
+            <input
+              type="checkbox"
+              data-testid={ `checkbox-music-${trackId}` }
+              id={ trackId }
+              checked={ CHECKED }
+              onChange={ () => this.handleCheckbox(music) }
+            />
+          </label>
+        </div>
+      )
     );
   }
 }
 
+export default MusicCard;
+
 MusicCard.propTypes = {
+  trackId: PropTypes.number.isRequired,
   trackName: PropTypes.string.isRequired,
   previewUrl: PropTypes.string.isRequired,
+  music: PropTypes.shape({
+    trackId: PropTypes.number.isRequired,
+    trackName: PropTypes.string.isRequired,
+    previewUrl: PropTypes.string.isRequired,
+  }).isRequired,
 };
-
-export default MusicCard;
